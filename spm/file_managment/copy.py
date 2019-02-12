9d258ff88aa4ad046c7bd59a53a0ed0b4b516611
Paweł <pawel.mstowski@gmail.com>
2019-02-12 20:53:46 +0100

    refactoring of copy file

--- spm/file_managment/copy.py
+++ spm/file_managment/copy.py
@@ -1,20 +1,37 @@
-from os import getcwd, makedirs
+from os import getcwd, makedirs, path
 from shutil import copyfile
 
 from spm.config import Config
 
 
+def get_path_from(config: Config):
+    return path.join(getcwd(), config.from_path)
+
+
+def get_path_to(config: Config):
+    return path.join(getcwd(), config.to_path)
+
+
+def get_file_name(config: Config, id: str):
+    return config.file_scheme.format(id)
+
+
 def __handle_file(config: Config, id):
+    file_name = get_file_name(config, id)
+    path_to = get_path_to(config)
+    path_from = get_path_from(config)
+
     print(
         'Coping file {} to {}'.format(
-            config.file_scheme.format(id), 
-            '{}/{}'.format(getcwd(),config.to_path)))
+            file_name, 
+            path_to))
+
     copyfile(
-        src='{}/{}/{}'.format(getcwd(),config.from_path, config.file_scheme.format(id)),
-        dst='{}/{}/{}'.format(getcwd(), config.to_path, config.file_scheme.format(id)))
+        src=path.join(path_from, file_name),
+        dst=path.join(path_to, file_name))
+
 
 def copy_files(config: Config):
-    makedirs('{}/{}'.format(getcwd(), config.to_path), exist_ok=True)
+    makedirs(get_path_to(config), exist_ok=True)
     for id in config.ids:
         __handle_file(config, id)
-    pass
