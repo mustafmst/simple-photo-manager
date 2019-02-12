9cd47402502cf0a4ae2a7c062afe9136172ecee3
Paweł <pawel.mstowski@gmail.com>
2019-02-12 20:38:14 +0100

    coping files to directory finished

--- spm/file_managment/copy.py
+++ spm/file_managment/copy.py
@@ -1,10 +1,20 @@
+from os import getcwd, makedirs
+from shutil import copyfile
+
 from spm.config import Config
 
 
-def __handle_file(scheme: str, id):
-    print(scheme.format(id))
+def __handle_file(config: Config, id):
+    print(
+        'Coping file {} to {}'.format(
+            config.file_scheme.format(id), 
+            '{}/{}'.format(getcwd(),config.to_path)))
+    copyfile(
+        src='{}/{}/{}'.format(getcwd(),config.from_path, config.file_scheme.format(id)),
+        dst='{}/{}/{}'.format(getcwd(), config.to_path, config.file_scheme.format(id)))
 
 def copy_files(config: Config):
+    makedirs('{}/{}'.format(getcwd(), config.to_path), exist_ok=True)
     for id in config.ids:
-        __handle_file(config.file_scheme, id)
+        __handle_file(config, id)
     pass
